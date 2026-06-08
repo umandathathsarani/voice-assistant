aa341e5685cbe90913aee44385116d85a25d0026
Umanda Thathsarani <umathathsarani2003@gmail.com>
2026-06-08 14:44:23 +0530

    Update TTS engine to use male system voice

--- modules/speech.py
+++ modules/speech.py
@@ -6,6 +6,11 @@ tts_engine = pyttsx3.init()
 tts_engine.setProperty('rate', 175)
 tts_engine.setProperty('volume', 1.0)
 
+voices = tts_engine.getProperty('voices')
+tts_engine.setProperty('voice', voices[0].id) 
+## 0 --> Microsoft David (Male) 
+## 1 --> Microsoft Zira (Female)
+
 def speak(text):
     print(f"{config.ASSISTANT_NAME}: {text}")
     tts_engine.say(text)
